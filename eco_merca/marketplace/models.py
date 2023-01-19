5864b4611137c0cdb08c5d703710e9be42f026ca
ChristianMartinezTech <crismartine.e@gmail.com>
2023-01-19 14:46:47 -0500

    Created the model of the ORM

--- eco_merca/marketplace/models.py
+++ eco_merca/marketplace/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    #brand_id = models.IntegerField
+    name = models.CharField(max_length=50)
+    product_quantity = models.IntegerField
+    instagram = models.CharField(max_length=100)
+
+class Products(models.Model):
+    #product_id = models.IntegerField
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField
+    price = models.FloatField
+    quantity = models.IntegerField
+    
