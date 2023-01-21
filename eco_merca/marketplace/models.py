6dcc744a20c0c8bf77b1b519ecb42e06667a7f26
ChristianMartinezTech <crismartine.e@gmail.com>
2023-01-21 16:21:56 -0500

    Finished models.py

--- eco_merca/marketplace/models.py
+++ eco_merca/marketplace/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
+
 class Brand(models.Model):
     #brand_id = models.IntegerField
     name = models.CharField(max_length=50)
     product_quantity = models.IntegerField
     instagram = models.CharField(max_length=100)
 
-class Products(models.Model):
+    # String method
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
     #product_id = models.IntegerField
     brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -14,4 +20,7 @@ class Products(models.Model):
     image = models.ImageField
     price = models.FloatField
     quantity = models.IntegerField
-    
+
+    # String method
+    def __str__(self):
+        return str(self.brand_id) + self.name + self.description + str(self.price) + str(self.quantity)
