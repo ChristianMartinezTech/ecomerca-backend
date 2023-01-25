dedffbc6d0d59ede38f598f268102486e55a0c92
aike-s <sagrilo804@gmail.com>
2023-01-25 16:10:57 -0500

    [add] url path and model changes

--- eco_merca/marketplace/models.py
+++ eco_merca/marketplace/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Brand(models.Model):
     #brand_id = models.IntegerField
     name = models.CharField(max_length=50)
-    product_quantity = models.IntegerField
+    city = models.CharField(max_length=50, default="remoto")
+    product_quantity = models.IntegerField(default=0)
     instagram = models.CharField(max_length=100)
 
     # String method
@@ -14,13 +15,13 @@ class Brand(models.Model):
 
 class Product(models.Model):
     #product_id = models.IntegerField
-    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    price = models.FloatField
-    quantity = models.IntegerField
+    price = models.FloatField(default=0)
+    quantity = models.IntegerField(default=0)
     #image = models.ImageField
 
     # String method
     def __str__(self):
-        return f'brand id:{self.brand_id} - Product Name:{self.name} - Quantity:@{self.quantity}'
+        return f'brand id:{self.brand_id} - Product Name:{self.name} - Price:{self.price} - Quantity:{self.quantity}'
--- eco_merca/marketplace/urls.py
+++ eco_merca/marketplace/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("brand/<int:brand_id>/", views.brand_detail, name="index"),
+    path("<int:brand_id>/", views.brand_detail, name="index"),
     path("product/<int:product_id>/", views.product_detail, name="index"),
 ]
