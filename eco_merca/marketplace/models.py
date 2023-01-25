7fe080c3f40a94869eaf88961e04f57698ea7877
Yasgc24 <yasmin.giraldo.castano@gmail.com>
2023-01-25 15:59:22 -0500

    Create models Brand, Product

--- eco_merca/marketplace/models.py
+++ eco_merca/marketplace/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    #brand_id = models.IntegerField
+    name = models.CharField(max_length=50)
+    product_quantity = models.IntegerField
+    instagram = models.CharField(max_length=100)
+
+    # String method
+    def _str_(self):
+        return f'Brand name:{self.name} - Product Quantity:{self.product_quantity} - Contact:@{self.instagram}'
+
+
+class Product(models.Model):
+    #product_id = models.IntegerField
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+    # String method
+    def _str_(self):
+        return f'brand id:{self.brand_id} - Product Name:{self.name} - Quantity:{self.quantity}'
